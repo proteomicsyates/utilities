13fef3c913fdc771d907a86fb2c476914c34cb70
Salvador Martinez-Bartolome <Salvador Martinez-Bartolome>
2017-02-03 17:00:53 -0800

    using different classes for XML validation

--- src/main/java/edu/scripps/yates/utilities/xml/XmlSchemaValidator.java
+++ src/main/java/edu/scripps/yates/utilities/xml/XmlSchemaValidator.java
@@ -8,11 +8,8 @@ import java.net.URI;
 import java.net.URL;
 
 import javax.xml.stream.FactoryConfigurationError;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.Source;
-import javax.xml.transform.stax.StAXSource;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
@@ -25,11 +22,11 @@ public class XmlSchemaValidator {
 	 * This static object is used to create the Schema object used for
 	 * validation.
 	 */
-	private static final SchemaFactory SCHEMA_FACTORY = SchemaFactory
-			.newInstance("http://www.w3.org/2001/XMLSchema");
+	private static final SchemaFactory SCHEMA_FACTORY = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
 
-	public static Schema getSchema(URI aSchemaUri) throws SAXException,
-			MalformedURLException {
+	static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+
+	public static Schema getSchema(URI aSchemaUri) throws SAXException, MalformedURLException {
 		return SCHEMA_FACTORY.newSchema(aSchemaUri.toURL());
 	}
 
@@ -45,22 +42,19 @@ public class XmlSchemaValidator {
 		return SCHEMA_FACTORY.newSchema(schemaSource);
 	}
 
-	public static XMLSchemaValidatorErrorHandler validate(Reader reader,
-			Schema schema) throws SAXException {
+	public static XMLSchemaValidatorErrorHandler validate(Reader reader, Schema schema) throws SAXException {
 
 		final XMLSchemaValidatorErrorHandler errorHandler = new XMLSchemaValidatorErrorHandler();
 		Validator validator = schema.newValidator();
 		validator.setErrorHandler(errorHandler);
 		try {
-			XMLStreamReader xmlStreamReader = XMLInputFactory.newInstance()
-					.createXMLStreamReader(reader);
-			validator.validate(new StAXSource(xmlStreamReader));
+			// XMLStreamReader xmlStreamReader =
+			// XMLInputFactory.newInstance().createXMLStreamReader(reader);
+			validator.validate(new StreamSource(reader));
 		} catch (IOException ioe) {
 			errorHandler.fatalError(ioe);
 		} catch (SAXParseException spe) {
 			errorHandler.fatalError(spe);
-		} catch (XMLStreamException e) {
-			e.printStackTrace();
 		} catch (FactoryConfigurationError e) {
 			e.printStackTrace();
 		}
