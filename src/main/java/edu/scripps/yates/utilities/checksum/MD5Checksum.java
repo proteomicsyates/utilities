62b9edf4295f1e2e0091edd2a9c82a78562bbc89
Salvador Martinez-Bartolome <salvador@scripps.edu>
2017-08-08 15:30:53 -0700

    new methods to get MD5 checksum from string

--- src/main/java/edu/scripps/yates/utilities/checksum/MD5Checksum.java
+++ src/main/java/edu/scripps/yates/utilities/checksum/MD5Checksum.java
@@ -1,13 +1,32 @@
 package edu.scripps.yates.utilities.checksum;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class MD5Checksum {
 
-	public static byte[] createChecksum(String filename) throws Exception {
+	public static byte[] createChecksumFromFile(String filename) throws Exception {
 		InputStream fis = new FileInputStream(filename);
+
+		return createChecksum(fis);
+	}
+
+	public static byte[] createChecksumFromFile(File file) throws Exception {
+		InputStream fis = new FileInputStream(file);
+
+		return createChecksum(fis);
+	}
+
+	public static byte[] createChecksumFromString(String string) throws Exception {
+		InputStream is = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+		return createChecksum(is);
+	}
+
+	public static byte[] createChecksum(InputStream fis) throws Exception {
 
 		byte[] buffer = new byte[1024];
 		MessageDigest complete = MessageDigest.getInstance("MD5");
@@ -26,14 +45,29 @@ public class MD5Checksum {
 
 	// see this How-to for a faster way to convert
 	// a byte array to a HEX string
-	public static String getMD5Checksum(String filename) throws Exception {
-		byte[] b = createChecksum(filename);
-		String result = "";
+	public static String getMD5ChecksumFromFileName(String filename) throws Exception {
+		byte[] b = createChecksumFromFile(filename);
+		return getStringFromBytes(b);
+	}
+
+	public static String getMD5ChecksumFromString(String string) throws Exception {
+		byte[] b = createChecksumFromString(string);
+		return getStringFromBytes(b);
+	}
+
+	public static String getMD5ChecksumFromFileName(File file) throws Exception {
+		byte[] b = createChecksumFromFile(file);
+		return getStringFromBytes(b);
+
+	}
+
+	public static String getStringFromBytes(byte[] b) {
+		StringBuilder result = new StringBuilder();
 
 		for (int i = 0; i < b.length; i++) {
-			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+			result.append(Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1));
 		}
-		return result;
+		return result.toString();
 	}
 
 }
