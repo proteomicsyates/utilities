86f83d935dc625baad791ab31f63841ee07838f4
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-04-27 13:50:39 -0700

    wrapper class to represent aminacids

---
+++ src/main/java/edu/scripps/yates/utilities/masses/UniprotAminoacid.java
@@ -0,0 +1,34 @@
+package edu.scripps.yates.utilities.masses;
+
+public class UniprotAminoacid {
+	private final char oneLetterCode;
+	private final String threeLetterCode;
+	private final String aminoacidName;
+	private Double monoMass;
+
+	public UniprotAminoacid(char oneLetterCode, String threeLetterCode, String aminoacidName) {
+		super();
+		this.oneLetterCode = oneLetterCode;
+		this.threeLetterCode = threeLetterCode;
+		this.aminoacidName = aminoacidName;
+	}
+
+	public char getOneLetterCode() {
+		return oneLetterCode;
+	}
+
+	public String getThreeLetterCode() {
+		return threeLetterCode;
+	}
+
+	public String getAminoacidName() {
+		return aminoacidName;
+	}
+
+	public Double getMonoMass() {
+		if (monoMass == null) {
+			monoMass = AssignMass.getInstance(true).getMass(oneLetterCode);
+		}
+		return monoMass;
+	}
+}
