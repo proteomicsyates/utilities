8bf9c8efa2efbb7be18664148b3034b864772da7
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-05-08 16:46:04 -0700

    new method to the multiple items from multiple keys: public List<T> getItems(Collection<String> keys);

--- src/main/java/edu/scripps/yates/utilities/index/FileIndex.java
+++ src/main/java/edu/scripps/yates/utilities/index/FileIndex.java
@@ -1,5 +1,7 @@
 package edu.scripps.yates.utilities.index;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -7,6 +9,8 @@ import edu.scripps.yates.utilities.util.Pair;
 
 public interface FileIndex<T> {
 	public T getItem(String key);
+
+	public List<T> getItems(Collection<String> keys);
 
 	public Map<String, Pair<Long, Long>> addItem(T item, Set<String> keys);
 
