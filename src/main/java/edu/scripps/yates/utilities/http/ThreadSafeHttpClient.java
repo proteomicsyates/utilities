c115213cdb28e112ee5a3afc92d488ce48a73929
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-05-15 09:26:38 -0700

    thread safe http client using connection pool

---
+++ src/main/java/edu/scripps/yates/utilities/http/ThreadSafeHttpClient.java
@@ -0,0 +1,60 @@
+package edu.scripps.yates.utilities.http;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.log4j.Logger;
+
+public class ThreadSafeHttpClient {
+	private final static Logger log = Logger.getLogger(ThreadSafeHttpClient.class);
+	private static PoolingHttpClientConnectionManager cm;
+	private static CloseableHttpClient client;
+
+	private static PoolingHttpClientConnectionManager getConnectionManager() {
+		if (cm == null) {
+			cm = new PoolingHttpClientConnectionManager();
+			// Increase max total connection to 200
+			cm.setMaxTotal(200);
+			// Increase default max connection per route to 20
+			cm.setDefaultMaxPerRoute(20);
+			// Increase max connections for localhost:80 to 50
+			final HttpHost localhost = new HttpHost("locahost", 80);
+			cm.setMaxPerRoute(new HttpRoute(localhost), 50);
+		}
+		return cm;
+	}
+
+	public static CloseableHttpClient createHttpClient() {
+		return HttpClients.custom().setConnectionManager(getConnectionManager()).build();
+
+	}
+
+	public static String getStringResponse(CloseableHttpClient httpClient, HttpGet httpGet)
+			throws ClientProtocolException, IOException {
+		try {
+
+			final CloseableHttpResponse response = httpClient.execute(httpGet, HttpClientContext.create());
+			if (response.getStatusLine().getStatusCode() == 200) {
+				final HttpEntity entity = response.getEntity();
+				final String ret = IOUtils.toString(entity.getContent());
+				return ret;
+			}
+			log.error("Error from http client. Status code:" + response.getStatusLine().getStatusCode() + ", Reason:"
+					+ response.getStatusLine().getReasonPhrase());
+			return null;
+		} finally {
+			httpGet.releaseConnection();
+		}
+
+	}
+}
