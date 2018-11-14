f7973d3ce5cd76c0fbf2ef1d6d20aa05b0c66a0a
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-11-13 16:18:11 -0800

    more consistent way to run and get result from command running

--- src/main/java/edu/scripps/yates/utilities/exec/CommandLineRunner.java
+++ src/main/java/edu/scripps/yates/utilities/exec/CommandLineRunner.java
@@ -9,28 +9,34 @@ import org.apache.log4j.Logger;
 
 public class CommandLineRunner {
 	private final static Logger log = Logger.getLogger(CommandLine.class);
+	private final MyProcessExecutorHandler handler = new MyProcessExecutorHandler();
+	private Long processExitCode;
 
-	public static Long runCommand(CommandLine commandLine, long timeout)
+	public void runCommand(CommandLine commandLine, long timeout)
 			throws IOException, InterruptedException, ExecutionException {
 		final String commandString = commandLine.toString();
 		log.info("Running: " + commandString);
-		ProcessExecutorHandler handler = new ProcessExecutorHandler() {
-			@Override
-			public void onStandardOutput(String msg) {
-				log.debug("OUTPUT:" + msg);
-			}
-
-			@Override
-			public void onStandardError(String msg) {
-				log.error("ERROR:" + msg);
-			}
-		};
+		handler.resetMesssages();
 		final Future<Long> runProcess = ProcessExecutor.runProcess(commandLine, handler, timeout);
 		while (!runProcess.isDone() && !runProcess.isCancelled()) {
 			Thread.sleep(1000);
 		}
-		final Long processExitCode = runProcess.get();
+		processExitCode = runProcess.get();
+		if (handler.containsErrorMessage()) {
+			processExitCode = -1l;
+		}
 		log.info("Process exitValue: " + processExitCode);
+	}
+
+	public String getErrorMessage() {
+		return handler.getErrorMessage();
+	}
+
+	public boolean containsErrorMessage() {
+		return handler.containsErrorMessage();
+	}
+
+	public Long getProcessExitCode() {
 		return processExitCode;
 	}
 }
