cd59d4ab212481c209ec8e2dfec10ad73449b264
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2019-07-25 17:09:29 -0700

    bidirectional mmaping between uniprot accs and genes

--- src/main/java/edu/scripps/yates/utilities/annotations/uniprot/UniprotGeneMapping.java
+++ src/main/java/edu/scripps/yates/utilities/annotations/uniprot/UniprotGeneMapping.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.net.ftp.FTPClient;
@@ -28,7 +29,8 @@ public class UniprotGeneMapping {
 	/**
 	 * Gene name to Uniprot accession map.
 	 */
-	private final Map<String, Set<String>> geneNameToAccession = new THashMap<String, Set<String>>();
+	private final Map<Gene, Set<String>> geneNameToAccession = new THashMap<Gene, Set<String>>();
+	private final Map<String, Set<Gene>> accessionToGeneNames = new THashMap<String, Set<Gene>>();
 	private boolean loaded = false;
 	private final File uniprotPath;
 	private final String taxonomy;
@@ -36,6 +38,9 @@ public class UniprotGeneMapping {
 	private static Map<String, UniprotGeneMapping> instances = new THashMap<String, UniprotGeneMapping>();
 	private static String uniprotFTPRemoteFolder = "/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/";
 	private static Logger log = Logger.getLogger(UniprotGeneMapping.class);
+	public static final String GENE_SYNONM = "Gene_Synonym";
+	public static final String GENE_NAME = "Gene_Name";
+	public static final String ENSEMBL = "Ensembl";
 
 	public UniprotGeneMapping(File uniprotPath, String taxonomy) {
 		this.uniprotPath = uniprotPath;
@@ -57,6 +62,47 @@ public class UniprotGeneMapping {
 			return geneNameToAccession.get(geneNameOrID);
 		}
 		return Collections.emptySet();
+	}
+
+	/**
+	 * Gets a Set of all gene names mapped to this uniprot acc
+	 * 
+	 * @param uniprotACC
+	 * @return
+	 * @throws IOException
+	 */
+	public Set<String> mapUniprotACCToGene(String uniprotACC) throws IOException {
+		if (!loaded) {
+			importFromFile(getMappingFile());
+		}
+		if (accessionToGeneNames.containsKey(uniprotACC)) {
+			final Set<Gene> genes = accessionToGeneNames.get(uniprotACC);
+			return genes.stream().map(gene -> gene.getName()).collect(Collectors.toSet());
+
+		}
+		return Collections.emptySet();
+	}
+
+	/**
+	 * Gets a map of all gene names mapped to this uniprot acc mapped by its
+	 * gene type
+	 * 
+	 * @param uniprotACC
+	 * @return
+	 * @throws IOException
+	 */
+	public Map<String, String> mapUniprotACCToGeneByType(String uniprotACC) throws IOException {
+		if (!loaded) {
+			importFromFile(getMappingFile());
+		}
+		if (accessionToGeneNames.containsKey(uniprotACC)) {
+			final Set<Gene> genes = accessionToGeneNames.get(uniprotACC);
+			final Map<String, String> map = new THashMap<String, String>();
+			genes.forEach(gene -> map.put(gene.getType(), gene.getName()));
+			return map;
+
+		}
+		return Collections.emptyMap();
 	}
 
 	private File getMappingFile() throws IOException {
@@ -137,31 +183,36 @@ public class UniprotGeneMapping {
 						final String accession = splittedLine[0];
 						final String type = splittedLine[1];
 						String geneName = null;
-						if (type.equals("Gene_Name")) {
+						if (type.equals(GENE_NAME)) {
 							geneName = splittedLine[2];
 							if (geneName != null) {
-								if (!geneNameToAccession.containsKey(geneName)) {
-									geneNameToAccession.put(geneName, new THashSet<String>());
+								final Gene gene = new Gene(geneName, type);
+								if (!geneNameToAccession.containsKey(gene)) {
+									geneNameToAccession.put(gene, new THashSet<String>());
 								}
-								geneNameToAccession.get(geneName).add(accession);
+								geneNameToAccession.get(gene).add(accession);
 							}
 						}
-						if (type.equals("Gene_Synonym")) {
+
+						if (type.equals(GENE_SYNONM)) {
 							geneName = splittedLine[2];
 							if (geneName != null) {
-								if (!geneNameToAccession.containsKey(geneName)) {
-									geneNameToAccession.put(geneName, new THashSet<String>());
+								final Gene gene = new Gene(geneName, type);
+								if (!geneNameToAccession.containsKey(gene)) {
+									geneNameToAccession.put(gene, new THashSet<String>());
 								}
-								geneNameToAccession.get(geneName).add(accession);
+								geneNameToAccession.get(gene).add(accession);
 							}
 						}
-						if (type.equals("Ensembl")) {
+
+						if (type.equals(ENSEMBL)) {
 							geneName = splittedLine[2];
 							if (geneName != null) {
-								if (!geneNameToAccession.containsKey(geneName)) {
-									geneNameToAccession.put(geneName, new THashSet<String>());
+								final Gene gene = new Gene(geneName, type);
+								if (!geneNameToAccession.containsKey(gene)) {
+									geneNameToAccession.put(gene, new THashSet<String>());
 								}
-								geneNameToAccession.get(geneName).add(accession);
+								geneNameToAccession.get(gene).add(accession);
 							}
 						}
 					}
@@ -171,10 +222,25 @@ public class UniprotGeneMapping {
 				br.close();
 				loaded = true;
 				log.info(geneNameToAccession.size() + " genes mapped to uniprot accessions");
+				getReverseMapping();
+				log.info(accessionToGeneNames.size() + " uniprot accessions mapped to genes");
 			}
 
 		} finally {
 			r.close();
 		}
+	}
+
+	private void getReverseMapping() {
+		for (final Gene gene : geneNameToAccession.keySet()) {
+			final Set<String> accs = geneNameToAccession.get(gene);
+			for (final String acc : accs) {
+				if (!accessionToGeneNames.containsKey(acc)) {
+					accessionToGeneNames.put(acc, new THashSet<Gene>());
+				}
+				accessionToGeneNames.get(acc).add(gene);
+			}
+		}
+
 	}
 }
