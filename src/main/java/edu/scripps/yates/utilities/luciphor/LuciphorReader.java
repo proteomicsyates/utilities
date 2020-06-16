66b2370fdb86883e1ca91d745e55e7ce7a5e9343
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-06-16 15:39:39 -0700

    getting FDR from Luciphor

--- src/main/java/edu/scripps/yates/utilities/luciphor/LuciphorReader.java
+++ src/main/java/edu/scripps/yates/utilities/luciphor/LuciphorReader.java
@@ -13,8 +13,11 @@ import edu.scripps.yates.utilities.fasta.FastaParser;
 import edu.scripps.yates.utilities.proteomicsmodel.PSM;
 import edu.scripps.yates.utilities.proteomicsmodel.factories.MSRunEx;
 import edu.scripps.yates.utilities.proteomicsmodel.factories.PSMEx;
+import edu.scripps.yates.utilities.proteomicsmodel.factories.ScoreEx;
 import edu.scripps.yates.utilities.proteomicsmodel.utils.KeyUtils;
+import gnu.trove.list.TDoubleList;
 import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.TObjectIntMap;
 import gnu.trove.map.hash.THashMap;
@@ -28,6 +31,10 @@ public class LuciphorReader {
 	private static final String PREDICTED_SEQUENCE = "Predicted_Sequence";
 	private static final String SCANS = "Scans";
 	private static final String FILE_NAMES = "Filenames";
+	private static final String Global_FDRs = "Global_FDRs";
+	private static final String Local_FDRs = "Local_FDRs";
+	public static final String LOCAL_FDR = "Local FDR";
+	public static final String GLOBAL_SCORE = "Global FDR";
 
 	public LuciphorReader(File luciphorFile) {
 		this.luciphorFile = luciphorFile;
@@ -62,6 +69,10 @@ public class LuciphorReader {
 				final String fileNames = split[indexesByHeader.get(FILE_NAMES)];
 				final List<String> fileNameList = getFileNames(fileNames);
 				final String chargeStates = split[indexesByHeader.get("Charge_States")];
+				final String globalFDRs = split[indexesByHeader.get(Global_FDRs)];
+				final TDoubleList globalFDRList = getFDRList(globalFDRs);
+				final String localFDRs = split[indexesByHeader.get(Local_FDRs)];
+				final TDoubleList localFDRList = getFDRList(localFDRs);
 				final TIntList chargeStatesList = getScanNumbers(chargeStates);
 				if (scanNumbersList.size() != fileNameList.size()) {
 					throw new IllegalArgumentException(
@@ -83,10 +94,19 @@ public class LuciphorReader {
 					psm.setMSRun(new MSRunEx(fileName, fileName));
 					psm.setScanNumber(String.valueOf(scanNumber));
 					psm.setChargeState(chargeState);
+					// FDR
+					final double localFDR = localFDRList.get(i);
+					final double globalFDR = globalFDRList.get(i);
+
+					psm.addScore(new ScoreEx(String.valueOf(localFDR), LOCAL_FDR, "PTM Localization FDR",
+							"PTM Localization FDR calculated in Luciphor"));
+					psm.addScore(new ScoreEx(String.valueOf(globalFDR), GLOBAL_SCORE, "PTM Localization FDR",
+							"PTM Localization FDR calculated in Luciphor"));
+
 					if (this.psms.containsKey(psm.getKey())) {
 						numLineswithRepeatedPSMs++;
 //						log.warn(psm.getKey() + "\t" + psm + "\t\t" + psms.get(psm.getKey()));
-						System.out.println(numLine + " " + line);
+						log.warn(numLine + " " + line);
 					}
 					this.psms.put(psm.getKey(), psm);
 				}
@@ -97,6 +117,15 @@ public class LuciphorReader {
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private TDoubleList getFDRList(String fdrList) {
+		final TDoubleList ret = new TDoubleArrayList();
+		final String[] split = fdrList.split(",");
+		for (final String fdr : split) {
+			ret.add(Double.valueOf(fdr));
+		}
+		return ret;
 	}
 
 	private List<String> getFileNames(String fileNames) {
@@ -112,7 +141,11 @@ public class LuciphorReader {
 		final TIntList ret = new TIntArrayList();
 		final String[] split = scans2.split(",");
 		for (final String scan : split) {
-			ret.add(Integer.valueOf(scan));
+			try {
+				ret.add(Integer.valueOf(scan));
+			} catch (final NumberFormatException e) {
+				e.printStackTrace();
+			}
 		}
 		return ret;
 	}
