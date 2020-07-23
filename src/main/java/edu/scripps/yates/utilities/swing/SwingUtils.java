eb3c7c8db2b8beb275e00fdf99b87bf625e196a8
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-07-23 13:55:07 -0700

    new util method for center component on the screen

--- src/main/java/edu/scripps/yates/utilities/swing/SwingUtils.java
+++ src/main/java/edu/scripps/yates/utilities/swing/SwingUtils.java
@@ -3,6 +3,7 @@ package edu.scripps.yates.utilities.swing;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.Window;
 
 public class SwingUtils {
 	public static int getFractionOfScreenHeightSize(double fraction) {
@@ -26,5 +27,12 @@ public class SwingUtils {
 		final Dimension preferredSize = new Dimension(SwingUtils.getFractionOfScreenWidthSize(widthFraction),
 				SwingUtils.getFractionOfScreenHeightSize(heightFraction));
 		component.setPreferredSize(preferredSize);
+	}
+
+	public static void centerOnScreen(Window window) {
+		window.pack();
+		final java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		final java.awt.Dimension dialogSize = window.getSize();
+		window.setLocation((screenSize.width - dialogSize.width) / 2, (screenSize.height - dialogSize.height) / 2);
 	}
 }
