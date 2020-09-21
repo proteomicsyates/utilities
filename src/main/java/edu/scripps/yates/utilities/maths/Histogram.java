f2a0c84b6133cec8a3881bff3fc920a7c39e169a
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-09-21 15:59:34 -0700

    moving functions about histograms from Maths to Histogram

--- src/main/java/edu/scripps/yates/utilities/maths/Histogram.java
+++ src/main/java/edu/scripps/yates/utilities/maths/Histogram.java
@@ -29,4 +29,30 @@ public class Histogram {
 	public static double getBinSize(TDoubleList data, int numBins) {
 		return (data.max() - data.min()) / numBins;
 	}
+
+	/**
+	 * This returns the Sturgis rule which determines the number of bins in an
+	 * histogram as 1+3.3Log10n.<br>
+	 * https://www.me.psu.edu/cimbala/me345/Lectures/Histograms.pdf#:~:text=an%20integer%20number%20of%20bins%2C%20we%20would%20use,10.9%20bins%2C%20which%20we%20round%20to%2011%20bins.
+	 * 
+	 * @param n the number of data points in the histogram
+	 * @return
+	 */
+	public static int getSturgisRuleForHistogramBins(int n) {
+		final int numberOfBins = Long.valueOf(Math.round(1 + 3.3 * Math.log10(n))).intValue();
+		return numberOfBins;
+	}
+
+	/**
+	 * This returns the Rice rule which determines the number of bins in an
+	 * histogram as 2*n^(1/3)<br>
+	 * https://www.me.psu.edu/cimbala/me345/Lectures/Histograms.pdf#:~:text=an%20integer%20number%20of%20bins%2C%20we%20would%20use,10.9%20bins%2C%20which%20we%20round%20to%2011%20bins.
+	 * 
+	 * @param n the number of data points in the histogram
+	 * @return
+	 */
+	public static int getRiceRuleForHistogramBins(int n) {
+		final int numberOfBins = Long.valueOf(Math.round(2 * Math.pow(n, 1.0 / 3.0))).intValue();
+		return numberOfBins;
+	}
 }
--- src/main/java/edu/scripps/yates/utilities/maths/Maths.java
+++ src/main/java/edu/scripps/yates/utilities/maths/Maths.java
@@ -638,29 +638,4 @@ public class Maths {
 		return cv;
 	}
 
-	/**
-	 * This returns the Sturgis rule which determines the number of bins in an
-	 * histogram as 1+3.3Log10n.<br>
-	 * https://www.me.psu.edu/cimbala/me345/Lectures/Histograms.pdf#:~:text=an%20integer%20number%20of%20bins%2C%20we%20would%20use,10.9%20bins%2C%20which%20we%20round%20to%2011%20bins.
-	 * 
-	 * @param n the number of data points in the histogram
-	 * @return
-	 */
-	public static int getSturgisRuleForHistogramBins(int n) {
-		final int numberOfBins = Long.valueOf(Math.round(1 + 3.3 * Math.log10(n))).intValue();
-		return numberOfBins;
-	}
-
-	/**
-	 * This returns the Rice rule which determines the number of bins in an
-	 * histogram as 2*n^(1/3)<br>
-	 * https://www.me.psu.edu/cimbala/me345/Lectures/Histograms.pdf#:~:text=an%20integer%20number%20of%20bins%2C%20we%20would%20use,10.9%20bins%2C%20which%20we%20round%20to%2011%20bins.
-	 * 
-	 * @param n the number of data points in the histogram
-	 * @return
-	 */
-	public static int getRiceRuleForHistogramBins(int n) {
-		final int numberOfBins = Long.valueOf(Math.round(2 * Math.pow(n, 1.0 / 3.0))).intValue();
-		return numberOfBins;
-	}
 }
