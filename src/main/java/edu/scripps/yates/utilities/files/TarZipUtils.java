9d80ab2042a62a41c4413affc173194eb0e9d27d
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-10-28 19:34:50 -0700

    zip utilities for tar and tar.gz

--- src/main/java/edu/scripps/yates/utilities/files/TarZipUtils.java
+++ src/main/java/edu/scripps/yates/utilities/files/TarZipUtils.java
@@ -2,17 +2,20 @@ package edu.scripps.yates.utilities.files;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
-import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
 
 import edu.scripps.yates.utilities.progresscounter.ProgressCounter;
 import edu.scripps.yates.utilities.progresscounter.ProgressPrintingType;
@@ -29,10 +32,11 @@ import edu.scripps.yates.utilities.progresscounter.ProgressPrintingType;
  *
  */
 public class TarZipUtils {
-	private final List<String> fileList;
-	private final File OUTPUT_ZIP_FILE;
-	private final File SOURCE_FOLDER; // SourceFolder path
+	private final static Logger log = Logger.getLogger(TarZipUtils.class);
+	private final OutputStream outputStream;
 	private final boolean compress;
+	private final File folderToCompress;
+	private boolean logFileCopyProgress = false;
 
 	/**
 	 * 
@@ -41,105 +45,122 @@ public class TarZipUtils {
 	 * @param compress         if true, zip compression will be applied and a tar.gz
 	 *                         file will be created. If false, a .tar file will be
 	 *                         created with no compression.
+	 * @throws IOException
 	 */
-	public TarZipUtils(File folderToCompress, File outputFile, boolean compress) {
-		fileList = new ArrayList<String>();
-		OUTPUT_ZIP_FILE = outputFile;
-		SOURCE_FOLDER = folderToCompress;
+	public TarZipUtils(File folderToCompress, File outputFile, boolean compress) throws IOException {
+		outputStream = Files.newOutputStream(outputFile.toPath());
 		this.compress = compress;
-		generateFileList(folderToCompress);
+		this.folderToCompress = folderToCompress;
 	}
 
-	public void zipIt() throws IOException {
-		final String source = SOURCE_FOLDER.getName();
-		OutputStream os = null;
-		BufferedOutputStream buffOut = null;
-		GzipCompressorOutputStream gzipzos = null;
-		TarArchiveOutputStream tarOut = null;
-		try {
-			os = Files.newOutputStream(OUTPUT_ZIP_FILE.toPath());
-			buffOut = new BufferedOutputStream(os);
-			if (compress) {
-				gzipzos = new GzipCompressorOutputStream(os);
-				tarOut = new TarArchiveOutputStream(gzipzos);
-			} else {
-				tarOut = new TarArchiveOutputStream(buffOut);
+	/**
+	 * 
+	 * @param folderToCompress
+	 * @param outputStream
+	 * @param compress         if true, zip compression will be applied and a tar.gz
+	 *                         file will be created. If false, a .tar file will be
+	 *                         created with no compression.
+	 */
+	public TarZipUtils(File folderToCompress, OutputStream outputStream, boolean compress) {
+		this.folderToCompress = folderToCompress;
+		this.outputStream = outputStream;
+		this.compress = compress;
+	}
+
+	public long transfer() throws IOException {
+		final TarArchiveOutputStream tarOut;
+		final long sent = 0l;
+
+		final BufferedOutputStream buffOut = new BufferedOutputStream(outputStream);
+		if (compress) {
+			final GzipCompressorOutputStream gzipzos = new GzipCompressorOutputStream(buffOut);
+			tarOut = new TarArchiveOutputStream(gzipzos);
+		} else {
+			tarOut = new TarArchiveOutputStream(buffOut);
+		}
+		final Path source = folderToCompress.toPath();
+		Files.walkFileTree(source, new FileVisitor<Path>() {
+
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				Objects.requireNonNull(dir);
+				Objects.requireNonNull(attrs);
+				return FileVisitResult.CONTINUE;
 			}
 
-			System.out.println("Output to Zip : " + OUTPUT_ZIP_FILE.getAbsolutePath());
-			FileInputStream in = null;
-			int numFile = 1;
-			for (final String file : fileList) {
-				System.out.println("File Added : " + file);
-
-//				final ZipEntry ze = new ZipEntry(source + File.separator + file);
-//				zos.putNextEntry(ze);
-				try {
-					final File inputFile = new File(SOURCE_FOLDER.getAbsolutePath() + File.separator + file);
-
-					final TarArchiveEntry tarEntry = new TarArchiveEntry(inputFile, source + File.separator + file);
-					tarOut.putArchiveEntry(tarEntry);
-
-					in = new FileInputStream(inputFile);
-					copy(inputFile.length(), FilenameUtils.getName(inputFile.getAbsolutePath()) + " (" + numFile + "/"
-							+ fileList.size() + ")", in, tarOut);
-
-				} finally {
-					in.close();
-					tarOut.closeArchiveEntry();
-					numFile++;
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Objects.requireNonNull(file);
+				Objects.requireNonNull(attrs);
+				if (attrs.isSymbolicLink()) {
+					return FileVisitResult.CONTINUE;
 				}
+//				if (file.getFileName().toString().contains("analy")) {
+//					return FileVisitResult.CONTINUE;
+//				}
+				final Path targetFile = source.relativize(file);
+				try {
+					final TarArchiveEntry tarEntry = new TarArchiveEntry(file.toFile(), targetFile.toString());
+
+					tarOut.putArchiveEntry(tarEntry);
+					copy(file, tarOut);
+
+					tarOut.closeArchiveEntry();
+
+					System.out.printf("file : %s%n", file);
+
+				} catch (final IOException e) {
+					System.err.printf("Unable to tar.gz : %s%n%s%n", file, e);
+				}
+
+				return FileVisitResult.CONTINUE;
 			}
 
-//			zos.closeEntry();
-			System.out.println("Folder successfully processed");
-
-		} finally {
-			try {
-//				zos.close();
-				tarOut.finish();
-			} catch (final IOException e) {
-				e.printStackTrace();
+			@Override
+			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+				System.err.printf("Unable to tar.gz : %s%n%s%n", file, exc);
+				return FileVisitResult.CONTINUE;
 			}
-		}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				Objects.requireNonNull(dir);
+				if (exc != null)
+					throw exc;
+				return FileVisitResult.CONTINUE;
+			}
+		});
+
+		tarOut.finish();
+		tarOut.close();
+		System.out.println("Folder successfully processed");
+		return sent;
+
 	}
 
-	private long copy(long total, String suffix, FileInputStream input, OutputStream output) throws IOException {
-		final int buffersize = 8024;
-		final byte[] buffer = new byte[buffersize];
-		int n = 0;
-		long count = 0;
-		final ProgressCounter c = new ProgressCounter(total, ProgressPrintingType.PERCENTAGE_STEPS, 0, true);
-		c.setSuffix(suffix);
-		while (-1 != (n = input.read(buffer))) {
-			output.write(buffer, 0, n);
-			c.increment(n);
-			final String printIfNecessary = c.printIfNecessary();
-			if (!"".equals(printIfNecessary)) {
-				System.out.println(printIfNecessary);
-			}
-			count += n;
-		}
-		return count;
+	protected long copy(Path file, TarArchiveOutputStream tarOut) throws IOException {
+		final long total = file.toFile().length();
+		final ProgressCounter counter = new ProgressCounter(total, ProgressPrintingType.PERCENTAGE_STEPS, 0, true);
+		counter.setSuffix(file.toString());
+//		Files.copy(file, tarOut);
 
+		final InputStream in = file.getFileSystem().provider().newInputStream(file);
+		final byte[] buffer = new byte[4 * 1024];
+		int read = 0;
+		long totalRead = 0l;
+		while ((read = in.read(buffer)) > 0) {
+			counter.increment(read);
+			final String printIfNecessary = counter.printIfNecessary();
+			if (logFileCopyProgress && !"".equals(printIfNecessary)) {
+				log.info(printIfNecessary);
+			}
+			tarOut.write(buffer, 0, read);
+			totalRead += read;
+		}
+		return totalRead;
 	}
 
-	private void generateFileList(File node) {
-		// add file only
-		if (node.isFile()) {
-			fileList.add(generateZipEntry(node.getAbsolutePath()));
-		}
-
-		if (node.isDirectory()) {
-			final String[] subNote = node.list();
-			for (final String filename : subNote) {
-				generateFileList(new File(node, filename));
-			}
-		}
-	}
-
-	private String generateZipEntry(String file) {
-		final String ret = file.substring(SOURCE_FOLDER.getAbsolutePath().length() + 1, file.length());
-		return ret;
+	public void setLogFileCopyProgress(boolean log) {
+		logFileCopyProgress = log;
 	}
 }
