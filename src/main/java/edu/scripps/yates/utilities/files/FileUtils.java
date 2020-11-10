7327bcf3ffc24f928790f52a1a5bd736cefe9b8a
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-11-10 14:23:10 -0800

    check invalid characters for file names and folder names

--- src/main/java/edu/scripps/yates/utilities/files/FileUtils.java
+++ src/main/java/edu/scripps/yates/utilities/files/FileUtils.java
@@ -734,4 +734,33 @@ public class FileUtils {
 		throw exception;
 	}
 
+	private static char[] invalidFileNameCharacters = { 34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+			14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47 };
+	private static char[] invalidFolderNameCharacters = { 34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+			14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
+	public static char[] getInvalidFileNameCharacters() {
+		return invalidFileNameCharacters;
+	}
+
+	public static char[] getInvalidFolderNameCharacters() {
+		return invalidFolderNameCharacters;
+	}
+
+	public static String checkInvalidCharacterNameForFileName(String fileName) {
+		String ret = fileName;
+		for (final char c : invalidFileNameCharacters) {
+			ret = ret.replace(c, '_');
+		}
+		return ret;
+	}
+
+	public static String checkInvalidCharacterNameForFolderName(String fileName) {
+		String ret = fileName;
+		for (final char c : invalidFolderNameCharacters) {
+			ret = ret.replace(c, '_');
+		}
+		return ret;
+	}
+
 }
